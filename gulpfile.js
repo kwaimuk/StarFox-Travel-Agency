f87027dbdb499e0484713079d8b7a1eaa564c01a
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-30 11:01:21 -1000

    1st phase

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('scss', () => {
+  return gulp.src('./scss/*.scss')
+    .pipe(scss({ errLogToConsole: true }))
+    .pipe(gulp.dest('./public/css'))
+})
+
+gulp.task('browserSync', () => {
+  return browserSync.init(['./scss', './public'], {
+    server: {
+      baseDir: './public'
+    }
+  })
+})
+
+gulp.task('watch', ['browserSync'], () => {
+  gulp.watch('./scss/**/*.scss', ['scss'])
+})
+
+gulp.task('default', ['watch', 'scss'])
